6358ad5d852e380a5401bb22fbc575ee597094d3
Tekrian <tetehbriand@gmail.com>
2026-05-02 16:12:25 +0200

    Correction du prototype de la méthode splitLigne pour inclure une référence constante et i;pl2;entqtion

--- include/CsvLoader.hpp
+++ include/CsvLoader.hpp
@@ -10,7 +10,7 @@ class CsvLoader {
         std::string cheminVilles;   // Chemin vers villes.csv
         std::string cheminTemps;    // Chemin vers le fichier temps.csv
 
-        std::vector<std::string> splitLigne(const std::string) const; // fonction privée qui lit les csv et le met les données en vecteur en éliminant les virgules
+        std::vector<std::string> splitLigne(const std::string&) const; // fonction privée qui lit les lignes csv et le met les données en vecteur en éliminant les virgules
     
     public :
         CsvLoader(const std::string&, const std::string&);
--- src/CsvLoader.cpp
+++ src/CsvLoader.cpp
@@ -1,4 +1,5 @@
 #include "CsvLoader.hpp"
+#include <ifstream>
 
 CsvLoader::CsvLoader(const std::string& ch_villes, const std::string& ch_temps)
     : cheminVilles(ch_temps), cheminTemps(ch_temps)
@@ -11,3 +12,13 @@ CsvLoader::~CsvLoader()
 
 }
 
+std::vector<std::string> CsvLoader::splitLigne(const std::string& ligne) const{
+    std::vector<std::string> str;
+    std::stringstream ss(ligne);
+    std::string caractere;
+
+    while(std::getline(ss, caractere, ','))
+        str.push_back(caractere);
+
+    return str;
+}
