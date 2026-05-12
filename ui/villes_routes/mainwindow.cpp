7bd35f2176e40088b9ae9f2cf8a4a221e746758d
Tekrian <tetehbriand@gmail.com>
2026-05-12 09:52:10 +0200

    création de la fenêtre vide

--- ui/villes_routes/mainwindow.cpp
+++ ui/villes_routes/mainwindow.cpp
@@ -2,6 +2,9 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-{}
+{
+    setWindowTitle("Temps de routes"); //Titre de la fenêtre
+    setMinimumSize(600, 700);
+}
 
 MainWindow::~MainWindow() = default;
