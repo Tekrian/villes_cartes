116e510ce9ab62fd4f3744c94dc4d66a1998208f
Tekrian <tetehbriand@gmail.com>
2026-05-12 13:28:20 +0200

    refactor: reorganize MainWindow class layout and initialization

--- ui/villes_routes/mainwindow.cpp
+++ ui/villes_routes/mainwindow.cpp
@@ -4,7 +4,27 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setWindowTitle("Temps de routes"); //Titre de la fenêtre
-    setMinimumSize(600, 700);
+    // setMinimumSize(600, 700); //
+
+    //Création du widget centarl
+    mainWidget = new QWidget(this);
+    setCentralWidget(mainWidget);
+
+    //layout vertical
+    layoutPrincipal = new QVBoxLayout();
+    mainWidget->setLayout(layoutPrincipal);
+
+    titre = new QLabel("Calculateur de temps de route ", this);
+    titre->setFont(QFont("Arial", 18));
+    titre->setAlignment(Qt::AlignCenter);
+    layoutPrincipal->addWidget(titre);
+
+    groupTrajet = new QGroupBox("Trouver un trajet", this);
+    QVBoxLayout *layoutTrajet = new QVBoxLayout(groupTrajet);
+    layoutTrajet->setSpacing(10);
+    layoutPrincipal->addWidget(groupTrajet);
+
+
 }
 
 MainWindow::~MainWindow() = default;
--- ui/villes_routes/mainwindow.h
+++ ui/villes_routes/mainwindow.h
@@ -2,10 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QGroupBox>
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+private :
+    QWidget *mainWidget; //Le conteneur principal
+    QVBoxLayout *layoutPrincipal; // l'objet pour organiser les widgets verticalement
+    QLabel *titre; //titre de l'application
+    QGroupBox *groupTrajet;
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
